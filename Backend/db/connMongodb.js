05ac7af5a2189a30d81207c0d4cea6c5f57ce8d0
Endelsab <141985348+Endelsab@users.noreply.github.com>
2024-03-28 19:17:18 +0800

    mong

--- Backend/db/connMongodb.js
+++ Backend/db/connMongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.WENDEL_URI);
+		await mongoose.connect(process.env.CONNECTION_STRING);
 		console.log("connected to ohmygosh");
 	} catch (error) {
 		console.log("pesti may error", error.message);
