bc04cc742567ac9daff5381857c34cf9400f3f52
Endelsab <141985348+Endelsab@users.noreply.github.com>
2024-03-29 13:10:58 +0800

    border style

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -6,8 +6,7 @@ const Login = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 
-	const { loading, login } = useLogin()
-	
+	const { loading, login } = useLogin();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -15,25 +14,25 @@ const Login = () => {
 	};
 
 	return (
-		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-			<div className="h-full w-full bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-300">
-				<h1 className="text-3xl font-bold text-center mt-3 text-purple-900">
+		<div className="  flex justify-center items-center h-screen">
+			<div className=" glowing_border  rounded-md ">
+				<h1 className="text-3xl text-sky-500 font-bold text-center mt-5 ">
 					Wendel's App
 				</h1>
 				<h3 className="text-2xl mt-5 font-semibold text-center text-gray-200">
 					Login
 				</h3>
 				<form onSubmit={handleSubmit}>
-					<div className="flex flex-col items-center justify-center">
+					<div className="flex flex-col items-center justify-center p-4">
 						<input
-							className="w-80 border h-10 bg-transparent outline-none ease-in-out transition-all mt-10 rounded-md hover:w-full"
+							className="glowing_border p-2  w-[270px]  h-10 bg-transparent outline-none  mt-10 rounded-md "
 							type="text"
 							placeholder="Enter username"
 							value={username}
 							onChange={(e) => setUsername(e.target.value)}
 						/>
 						<input
-							className="w-80 border h-10 bg-transparent ease-in-out hover:w-full transition-all outline-none mt-5 rounded-md"
+							className=" glowing_border p-2 w-[270px]  h-10 bg-transparent outline-none mt-5 rounded-md"
 							type="password"
 							placeholder="Enter password"
 							value={password}
@@ -41,10 +40,14 @@ const Login = () => {
 						/>
 						<Link
 							to={"/signup"}
-							className="mt-3 hover:underline cursor-pointer">
+							className="mt-3 hover:underline text-sky-500 cursor-pointer">
 							Don't have an account ?
 						</Link>
-						<button disabled={loading} className="btn btn-primary my-5">Login</button>
+						<button
+							disabled={loading}
+							className="btn glowing_border bg-sky-500 my-5 text-black hover:bg-gray-500">
+							Login
+						</button>
 					</div>
 				</form>
 			</div>
--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -23,51 +23,53 @@ const SignUp = () => {
 	};
 
 	return (
-		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
-			<div className="h-full w-full bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-300">
-				<h1 className="text-3xl font-bold text-center mt-3 text-purple-900">
+		<div className="flex justify-center items-center h-screen">
+			<div className="glowing_border  rounded-md">
+				<h1 className="text-3xl font-bold text-center mt-4 text-sky-500">
 					Wendel's App
 				</h1>
-				<h3 className="text-2xl mt-5 font-semibold text-center text-gray-200">
+				<h3 className="text-2xl  mt-5 font-semibold text-center text-gray-200">
 					SignUp
 				</h3>
 				<form onSubmit={handleSubmit}>
-					<div className="flex flex-col items-center  justify-center">
+					<div className="flex flex-col items-center justify-center p-4">
 						<input
-							className="w-80 border h-10 bg-transparent hover:w-full ease-in-out transition-all  outline-none mt-10 rounded-md"
+							className=" glowing_border w-[270px]  p-2 h-10 bg-transparent outline-none mt-10 rounded-md"
 							type="email"
 							placeholder="Enter your email"
 							name="email"
 							onChange={handleChange}
 						/>
 						<input
-							className="w-80 border h-10 bg-transparent outline-none mt-5  hover:w-full ease-in-out transition-all rounded-md"
+							className=" glowing_border w-[270px] p-2 h-10 bg-transparent outline-none mt-5 ease-in-out  rounded-md"
 							type="text"
 							placeholder="Enter your username"
 							name="username"
 							onChange={handleChange}
 						/>
 						<input
-							className="w-80  hover:w-full ease-in-out transition-all border h-10 bg-transparent outline-none mt-5 rounded-md"
+							className=" glowing_border w-[270px] p-2  h-10 bg-transparent outline-none mt-5 rounded-md"
 							type="password"
 							placeholder="Enter your password"
 							name="password"
 							onChange={handleChange}
 						/>
 						<input
-							className="w-80  hover:w-full ease-in-out transition-all border h-10 bg-transparent outline-none mt-5 rounded-md"
+							className=" glowing_border w-[270px]  p-2 h-10 bg-transparent outline-none mt-5 rounded-md"
 							type="text"
 							placeholder="Enter your gender"
 							name="gender"
 							onChange={handleChange}
 						/>
 
-						<Link to={"/login"} className="mt-3 hover:underline cursor-pointer">
+						<Link
+							to={"/login"}
+							className="mt-3 text-sky-500 hover:underline cursor-pointer">
 							Already have an account ?
 						</Link>
 						<button
 							type="submit"
-							className="btn btn-primary my-5"
+							className="btn glowing_border bg-sky-500 my-5 text-black hover:bg-gray-500"
 							disabled={loading}>
 							SignUp
 						</button>
